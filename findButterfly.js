d15be7b5f8689899c1be483703febca3c9fcafdf
Keysha Cruz <keyshacruz00@gmail.com>
2023-09-03 17:13:52 -0400

    Adding file to repo

---
+++ findButterfly.js
@@ -0,0 +1,24 @@
+function onloadFunction() {
+    var amount = 1;
+    var arrayIDs = ["butterfly"]; 
+  
+    for (i=1;i<=amount;i++) {
+  
+     var element = document.getElementById(arrayIDs[i-1]);
+     var positionInfo = element.getBoundingClientRect();
+     var imgHeight = positionInfo.height;
+     var imgWidth = positionInfo.width;
+  
+    
+     var screenWidth = window.innerWidth;
+     var screenHeight = window.innerHeight;
+  
+     var imgLeft = Math.floor(Math.random() * (screenWidth - imgWidth));
+     var imgTop= Math.floor(Math.random() * (screenHeight - imgHeight));
+  
+     document.getElementById(arrayIDs[i-1]).style.top = imgTop+"px";
+     document.getElementById(arrayIDs[i-1]).style.left = imgLeft+"px";
+    }
+  }
+  
+  onloadFunction();
